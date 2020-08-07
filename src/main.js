78c0ec29737fdf1ba861d87fb3ef3be025caf19d
HayBeeCoder <Oguntadeabass1@gmail.com>
2020-08-07 23:03:47 +0100

    added mobile menu functionality

---
+++ src/main.js
@@ -0,0 +1,19 @@
+const menu_open = document.querySelector('.header__menu-open')
+const menu_close = document.querySelector('.header__menu-close')
+const mobile_menu = document.querySelector('.header-mobile')
+const header_logo = document.querySelector('.header__logo')
+
+function openMenu() {
+    mobile_menu.style.display = 'block';
+    menu__open.style.display = 'none';
+    header_logo.style.display = 'none'
+    body.style.overflow = 'hidden';
+}
+
+function closeMenu() {
+    mobile_menu.style.display = 'none';
+    menu__open.style.display = 'block';
+    header_logo.style.display = 'block'
+}
+menu_open.addEventListener('click', openMenu)
+menu_close.addEventListener('click', closeMenu)
