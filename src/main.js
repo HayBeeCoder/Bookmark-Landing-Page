17c0494a62b30981ea9f1bdbc785f8af497f80a3
HayBeeCoder <Oguntadeabass1@gmail.com>
2020-08-08 01:23:34 +0100

    made the accordion functional

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-const menu_open = document.querySelector('.header__menu-open')
-const menu_close = document.querySelector('.header__menu-close')
-const mobile_menu = document.querySelector('.header-mobile')
-const header_logo = document.querySelector('.header__logo')
+const menu_open = document.querySelector('.header__menu-open');
+const menu_close = document.querySelector('.header__menu-close');
+const mobile_menu = document.querySelector('.header-mobile');
+const header_logo = document.querySelector('.header__logo');
+const faq_buttons = document.getElementsByClassName('faq__arrow');
+const faq_paragraphs = document.getElementsByClassName('faq__paragraph');
+const arrow_svg = document.querySelector('path');
+const faqs = document.getElementsByClassName('faq')
 
 function openMenu() {
     mobile_menu.style.display = 'flex';
@@ -14,6 +18,25 @@ function closeMenu() {
     mobile_menu.style.display = 'none';
     menu_open.style.display = 'block';
     header_logo.style.display = 'block'
+    document.body.style.overflowY = 'scroll';
 }
 menu_open.addEventListener('click', openMenu)
 menu_close.addEventListener('click', closeMenu)
+
+
+
+
+for (let i = 0; i < faq_buttons.length; i++) {
+    let faq_button = faq_buttons[i]
+    faq_button.addEventListener('click', function() {
+        this.classList.toggle('faq__arrow_active');
+
+        let faq__paragraph = faq_paragraphs[i];
+        let faq = faqs[i];
+        if (faq__paragraph.style.display == 'block') {
+            faq__paragraph.style.display = 'none';
+            faq.style.color = 'initial';
+        } else { faq__paragraph.style.display = 'block' }
+
+    })
+}
